bb3eea8f97e9a5d1b04f8959feda9ecd7888e980
jaden.taylor <jadentaylor@jpisolutions.ca>
2026-02-15 21:12:12 -0700

    Added ability for queen to place nest block

--- Assets/Components/Agents/Queen.cs
+++ Assets/Components/Agents/Queen.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Antymology.Terrain;
 
 namespace Antymology.Agents
 {
@@ -8,22 +9,35 @@ namespace Antymology.Agents
         {
             tryDown(); // Make sure ants don't float if another ant dug below.
             checkAcid();
-            manageHealth();
+            damage(healthDecayRate * healthDecayMultiplier);
 
             forward();
+            if (currentHealth > maxHealth * 0.5f) {
+                createNestBlock();
+            } else
+            {
+                consumeMulchBlockBelow();
+            }
+            
 
             ticksElapsed++;
         }
         
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        /// <summary>
+        /// Queen ants can create a nest block where they standing, which will spawn new ants.
+        /// They will place the nest down and then move on top of it.
+        /// </summary>
+        public void createNestBlock()
         {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+            if (lookUp() is AirBlock)
+            {
+                // TODO: Check if there is an another ant on the block.
+                // Can only create a nest if there is air above the ant to prevent ants from creating nests that would put them inside the block
+                Vector3 currentPosition = transform.position + new Vector3(0, 1, 0);
+                WorldManager.Instance.SetBlock((int)currentPosition.x, (int)currentPosition.y, (int)currentPosition.z, new NestBlock());
+                damage(maxHealth / 3); // Reduce health by 1/3 when creating a nest.
+                tryUp();
+            }
             
         }
     }
